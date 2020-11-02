fe2efdbbf8f956926e6ea5e009ed4359ed05795a
MUHAMMED NIZAR K <nizar@iocod.com>
2020-11-02 11:21:58 +0530

    env file ignored

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const { merge } = require('webpack-merge');
+const { merge } = require('webpack-merge')
 
-const commonConfig = require('./config/webpack.common');
-const prodConfig = require('./config/webpack.prod');
-const devConfig = require('./config/webpack.dev');
+const commonConfig = require('./config/webpack.common')
+const prodConfig = require('./config/webpack.prod')
+const devConfig = require('./config/webpack.dev')
 
 module.exports = () => {
-    const env = process.env.NODE_ENV;
+    const env = process.env.NODE_ENV
     switch (env) {
         case 'development':
-            return merge(commonConfig, devConfig);
+            return merge(commonConfig, devConfig)
         case 'production':
-            return merge(commonConfig, prodConfig);
+            return merge(commonConfig, prodConfig)
         default:
-            throw new Error('No matching configuration was found!');
+            throw new Error('No matching configuration was found!')
     }
 }
